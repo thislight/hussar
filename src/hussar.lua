7fe82176bd2726a504d312316d2875bc68e71352
thisLight <l1589002388@gmail.com>
2020-09-15 13:13:35 +0800

    hussar: accept_connection

--- src/hussar.lua
+++ src/hussar.lua
@@ -1,5 +1,4 @@
 local away = require "away"
-local lphr = require "lphr"
 local httputil = require "hussar.httputil"
 local table_deep_copy = require("hussar.utils").table_deep_copy
 
@@ -9,61 +8,58 @@ function hussar:clone_to(new_t)
     return table_deep_copy(self, new_t)
 end
 
+local function new_error_thread(f, hussar_t)
+    local th = coroutine.create(f)
+    coroutine.resume(th, hussar_t)
+    return th
+end
+
 function hussar:create()
     return self:clone_to {
         sources = {},
         td = {},
+        pubframe = {},
+        error_thread = new_error_thread(function(self)
+            while true do
+                local signal = coroutine.yield()
+                error(signal.err, 0)
+                httputil.respond_on(signal.connection) {
+                    status_code = 500,
+                    "Server Internal Error"
+                }
+            end
+        end),
     }
 end
 
 function hussar:attach_source(source)
-    source:listen(function(conn)
-        hussar:accept_connection(conn)
-    end)
     table.insert(self.sources, source)
 end
 
-local function reshape_headers(old, new)
-    for _, t in ipairs(old) do
-        local key = t[1]
-        local value = t[2]
-        if not new[key] then
-            new[key] = {}
-        end
-        table.insert(new[key], value)
-    end
-    return new
-end
-
-local function conn_hander(conn, hussar_ins)
-    local protocol_data = {}
-    while conn:can_read() do
-        lphr.parse_request(conn:read(), protocol_data)
-    end
-    if protocol_data.prev_pret > 0 then
-        local body = lphr.get_body(protocol_data)
-        local request = {
-            method = protocol_data.method,
-            path = protocol_data.path,
-            headers = reshape_headers(protocol_data.headers, {}),
-            body = body,
+local function conn_hander(conn, pubframe, handler, error_thread)
+    local frame = {}
+    coroutine.yield() -- break point, only run handler after scheduled
+    local stat, err = pcall(handler, conn, frame, pubframe)
+    if not stat then
+        coroutine.yield {
+            target_thread = error_thread,
+            connection = conn,
+            err = err,
+            frame = frame,
+            pubframe = pubframe,
+            handler = handler,
         }
-        local response = {}
-        local handle = nil -- TODO
-        hussar_ins:handle_request(request, response, handle)
-        if response.body then
-            conn:write()
-        end
     else
-        conn:write(httputil.response {
-            code = 400,
-        })
+        if not conn:set_keep_alive() then
+            conn:close()
+        end
     end
 end
 
 function hussar:accept_connection(conn)
     local new_thread = coroutine.create(conn_hander)
-    coroutine.resume(new_thread, conn)
+    table.insert(self.td, {conn, new_thread})
+    coroutine.resume(new_thread, conn, self.pubframe, self.handler, self.error_thread)
     away.schedule_thread(new_thread)
 end
 
