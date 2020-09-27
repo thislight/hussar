d0c4328f4b7357bbfdcd92dbbd5b3ec281b93a67
thisLight <l1589002388@gmail.com>
2020-09-27 21:31:09 +0800

    add tests for source.fake

--- spec/fake_source_spec.lua
+++ spec/fake_source_spec.lua
@@ -1,7 +1,39 @@
 insulate("hussar.source.fake", function()
     local fake = require "hussar.source.fake"
-    describe("can created as a object", function()
+    it("can be created as a object", function()
         local obj = fake:create()
         assert.is.truthy(obj)
     end)
+
+    describe("add_request()", function()
+        it("returns a connection after been called", function()
+            local conn = fake:add_request {
+                path = '/',
+                method = 'GET',
+            }
+            assert.are.equals(type(conn), 'table')
+            assert.is.truthy(conn.read) -- a smoke test
+        end)
+    end)
+
+    describe("pull()", function()
+        it("returns empty table when no new connection", function()
+            local obj = fake:create()
+            local result = obj:pull()
+            assert.are.equals(type(result), 'table')
+            assert.are.equals(#result, 0)
+        end)
+
+        it("returns new connections after called add_request()", function()
+            local obj = fake:create()
+            local conn = obj:add_request {
+                path = '/',
+                method = 'GET'
+            }
+            local result = obj:pull()
+            assert.are.equals(#result, 1)
+            assert.are.same(conn, result[1])
+        end)
+    end)
+
 end)
