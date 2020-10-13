aa71b0016461cb557137c2f7d048ecb8e96cb0f1
thisLight <l1589002388@gmail.com>
2020-10-13 22:04:58 +0800

    source.fake: reconstructed

--- src/source/fake.lua
+++ src/source/fake.lua
@@ -3,102 +3,103 @@ local utils = require "hussar.utils"
 local httputil = require "hussar.httputil"
 local Dataqueue = require "away.dataqueue"
 
-local fake_connection = {}
-
-function fake_connection:clone_to(new_t)
-    return utils.table_deep_copy(self, new_t)
-end
-
-function fake_connection:create()
-    return self:clone_to {
-        flag_keep_alive = false,
-        buffer_in = {},
-        pos_in = 0,
-        buffer_out = {},
-        pos_out = 0,
-        is_alive = true,
+local function create_fake_connection_buffer(hold_until)
+    return {
+        pointer = 0,
+        alive = true,
+        keep_alive = false,
+        read = function(self)
+            local new_pos = self.pointer + 1
+            if #self >= new_pos then
+                self.pointer = new_pos
+                return self[new_pos]
+            else
+                hold_until(function()
+                    return #self > self.pointer
+                end)
+                return self:read()
+            end
+        end,
+        write = function(self, value)
+            if self.alive then
+                table.insert(self, value)
+            else
+                error({
+                    t = 'closed',
+                    r = self.closed_reason
+                })
+            end
+        end,
+        is_alive = function(self)
+            return self.alive
+        end,
+        close = function(self, reason)
+            self.alive = false
+            self.closed_reason = reason
+        end,
+        set_keep_alive = function(self, enable)
+            self.keep_alive = enable
+        end,
+        is_keep_alive = function(self)
+            return self.keep_alive
+        end,
     }
 end
 
----- Standard APIs ----
--- TODO: move to use of dataqueue
-function fake_connection:read()
-    local new_pos = self.pos_in + 1
-    if #self.buffer_in >= new_pos then
-        self.pos_in = new_pos
-        return self.buffer_in[new_pos]
-    else
-        utils.hold_until(function()
-            return #self.buffer_in > self.pos_in
-        end)
-        return fake_connection:read()
-    end
+local function create_connection_object(buffer, remote)
+    return {
+        buffer = buffer,
+        remote = remote,
+        read = function(self)
+            if not self.buffer:is_alive() then
+                error({
+                    t = 'closed',
+                    r = self.buffer.closed_reason
+                })
+            end
+            return self.buffer:read()
+        end,
+        write = function(self, value)
+            if not self.buffer:is_alive() then
+                error {
+                    t = 'closed',
+                    r = self.buffer.closed_reason
+                }
+            end
+            return self.remote:write(value)
+        end,
+        set_keep_alive = function(self, enable)
+            if self.buffer:is_alive() then
+                self.buffer:set_keep_alive(enable)
+                self.remote:set_keep_alive(enable)
+            else
+                error({
+                    t = 'closed',
+                    r = self.buffer.closed_reason
+                })
+            end
+        end,
+        close = function(self, reason)
+            if self.buffer:is_alive() then
+                self.buffer:close(reason)
+                self.remote:close()
+            end
+        end,
+        is_alive = function(self)
+            return self.buffer:is_alive()
+        end,
+        is_keep_alive = function(self)
+            return self.buffer:is_keep_alive()
+        end,
+    }
 end
 
-function fake_connection:write(value)
-    if self.is_alive then
-        table.insert(self.buffer_out, value)
-        return #value
-    else
-        error({
-            t = 'closed',
-            r = self.closed_reason,
-        })
-    end
-end
-
-function fake_connection:set_keep_alive(enable)
-    local old = self.flag_keep_alive
-    if enable ~= nil then
-        self.flag_keep_alive = enable
-    end
-    return old
-end
-
-function fake_connection:is_keep_alive()
-    return self.flag_keep_alive
-end
-
-function fake_connection:is_alive()
-    return self.is_alive
-end
-
-function fake_connection:close(reason)
-    self.is_alive = false
-    self.closed_reason = reason
-end
---------
-
-function fake_connection:client_close()
-    self.is_alive = false
-end
-
-function fake_connection:client_write(s)
-    if self.is_alive then
-        table.insert(self.buffer_in, s)
-    else
-        error({
-            t = 'closed',
-            r = self.closed_reason,
-        })
-    end
-end
-
-function fake_connection:client_read()
-    local new_pos = self.pos_out + 1
-    if #self.buffer_out > new_pos then
-        self.pos_out = new_pos
-        return self.buffer_in[new_pos]
-    else
-        utils.hold_until(
-            function() return #self.buffer_out > new_pos end
-        )
-        return self:client_read()
-    end
-end
-
-function fake_connection:client_is_keep_alive()
-    return self:is_keep_alive()
+local function create_fake_connection_pair()
+    local server_side_buffer = create_fake_connection_buffer(utils.hold_until)
+    local client_side_buffer = create_fake_connection_buffer(utils.hold_until)
+    local server_side_conn = create_connection_object(server_side_buffer, client_side_buffer)
+    local client_side_conn = create_connection_object(client_side_buffer, server_side_buffer)
+    return server_side_conn, client_side_conn
 end
 
 local fake_source = {
@@ -118,9 +119,9 @@ function fake_source:pull()
 end
 
 function fake_source:new_connection()
-    local conn = fake_connection:create()
-    table.insert(self.new_connections, conn)
-    return conn
+    local server_side, client_side = create_fake_connection_pair()
+    table.insert(self.new_connections, server_side)
+    return client_side
 end
 
 function fake_source:add_request(t)
