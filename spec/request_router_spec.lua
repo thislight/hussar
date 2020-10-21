4f8b86143ded5b982b202ece6b1b36ba566a12c8
thisLight <l1589002388@gmail.com>
2020-10-21 20:33:29 +0800

    update tests for request router

--- spec/request_router_spec.lua
+++ spec/request_router_spec.lua
@@ -1,25 +1,43 @@
-local away = require "away"
-local debugger = require "away.debugger"
-local RequestRouter = require "hussar.request_router"
-local checkers = require "hussar.request_router.checkers"
-
 describe("hussar.checkers", function()
+    local away = require "away"
+    local debugger = require "away.debugger"
+    local RequestRouter = require "hussar.request_router"
+    local checkers = require "hussar.request_router.checkers"
     describe("PATH", function()
         it("return true if request's path is match the rule", function()
             local checker = checkers.PATH("/te(.*)", 'lua')
-            local fake_request = {
-                path = '/test'
-            }
+            local fake_request = {path = '/test'}
             assert.is.True(checker(fake_request))
         end)
-        it("set capture to frame if reuqest's path is match the rule", function()
+        it("set capture to frame if reuqest's path is match the rule",
+           function()
             local checker = checkers.PATH("/te(.*)", 'lua')
-            local fake_request = {
-                path = '/test'
-            }
+            local fake_request = {path = '/test'}
             local frame = {}
             assert.is.True(checker(fake_request, frame))
             assert.equals(frame.path_match[1], 'st')
+        end)
+    end)
+end)
+
+describe("hussar.request_router", function()
+    local away = require "away"
+    local debugger = require "away.debugger"
+    local RequestRouter = require "hussar.request_router"
+    local checkers = require "hussar.request_router.checkers"
+    local mocks = require "away.debugger.mocks"
+    describe("route()", function()
+        it("can return correct handler by request", function()
+            local fake_request = {path = "/test"}
+            local mock_handler1 = mocks.callable()
+            local mock_handler2 = mocks.callable()
+            local router = RequestRouter:create(
+                               {
+                    {'/test$', mock_handler1.mock},
+                    {'/amdyes$', mock_handler2.mock}
+                })
+            local result = router:route(fake_request)
+            assert.equals(mock_handler1.mock, result)
         end)
     end)
 end)
