9221d1ee63e236ec0741bb98eaf3c813f66b6110
thisLight <l1589002388@gmail.com>
2020-10-24 21:29:56 +0800

    request_router: cleaned design

--- src/request_router/init.lua
+++ src/request_router/init.lua
@@ -23,33 +23,10 @@ local powerlog = require "powerlog"
 local terr = require "hussar.terr"
 local checkers = require "hussar.request_router.checkers"
 
-local wait_for_request = httputil.wait_for_request
-
 local RequestRouter = {
     routes = {},
-    logger = powerlog:create("hussar.request_router")
+    logger = powerlog:create("hussar.request_router"),
 }
-
-local function wrap_error_handler(f, error_handler)
-    return function(...) xpcall(f, wrapline.wrap_context(error_handler, ...), ...) end
-end
-
-local function make_handler(rules_f, error_handler)
-    return wrap_thread(wrap_error_handler(
-                           function(conn, frame, pubframe)
-            local request = wait_for_request(conn)
-            frame.connection = conn
-            frame.request = request
-            local user_handler = rules_f(request)
-            local result = user_handler(request, frame, pubframe)
-            if result and conn:is_alive() then
-                httputil.respond(conn, result)
-            end
-            if (not conn:is_keep_alive()) and (conn:is_alive()) then
-                conn:close()
-            end
-        end, error_handler))
-end
 
 function RequestRouter:clone_to(t)
     return utils.table_deep_copy(self, t)
@@ -57,6 +34,7 @@ end
 
 function RequestRouter:create(routes)
     local newobj = self:clone_to {}
+    newobj.wrapline = wrapline.create()
     newobj:add_routes(routes or {})
     return newobj
 end
@@ -71,38 +49,21 @@ function RequestRouter:add_routes(routes)
     end
 end
 
-function RequestRouter:attach(hussar)
-    hussar.handler = make_handler(function(request) self:route(request) end, wrapline.wrap_context(self.error_handler, self))
-end
-
-function RequestRouter:route(request)
+function RequestRouter:route(request, conn, frame, pubframe)
     for i, v in ipairs(self.routes) do
         local handler, checker = table.unpack(v)
-        if checker(request) then
+        if checker(request, frame) then
             return handler
         end
     end
-    return self.not_found_error_handler
+    return wrapline.wrap_context(self.not_found_error_handler, request)
 end
 
-function RequestRouter.not_found_error_handler(request, frame, pubframe)
+function RequestRouter.not_found_error_handler(request)
     return {
         status = 404,
         string.format("404 Not Found: %s", request.path)
     }
-end
-
-function RequestRouter:error_handler(e, conn, frame, pubframe)
-    if terr.is(e) then
-        if frame.request then
-            e.e = e.e or {}
-            if type(e.e) == 'table' then
-                e.e.request = frame.request
-            end
-        end
-    end
-    httputil.respond(conn, {status=500})
-    self.logger:warn("error while handler runing", e)
 end
 
 return RequestRouter
