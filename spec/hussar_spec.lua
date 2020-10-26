eb1f894daa9dde509bfacb2fee30f0a805d80df8
thisLight <l1589002388@gmail.com>
2020-10-26 13:29:59 +0800

    update testcase for hussar

--- spec/hussar_spec.lua
+++ spec/hussar_spec.lua
@@ -6,17 +6,19 @@ describe("hussar", function()
     local Hussar = require "hussar"
     local httputil = require "hussar.httputil"
     local FakeSource = require "hussar.source.fake"
+    local wrap_thread = require "hussar.wrap_thread"
+    local lphrc = require "lphr.c"
 
     it("can build a server and play with fake_source correctly", function()
         local handler = mocks.callable(
-            function(conn)
+            wrap_thread(function(conn)
                 local request = httputil.wait_for_request(conn)
                 assert.equals(type(request), "table")
                 httputil.respond_on(conn) {
                     status = 200,
                     request.path
                 }
-            end
+            end)
         )
         local server = Hussar:create()
         local source = FakeSource:create()
@@ -28,9 +30,13 @@ describe("hussar", function()
                 local conn = source:add_request {
                     method = 'GET',
                     path = '/',
-                    ['Transfer-Encoding'] = "Chunked",
                 }
-                print(conn:read())
+                local result = conn:read_and_wait()
+                local pret, last_len, data
+                pret, last_len, data = lphrc.parse_response_r2(result, last_len)
+                assert.is.True(pret > 0, "fake_connection must always return the strings as it is")
+                assert.is.truthy(string.match((string.sub(result, last_len)), "%/$"))
+                scheduler:stop()
             end)
             scheduler:run_task(function()
                 server:start()
