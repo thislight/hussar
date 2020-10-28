83dc52d7d74e16530b66e78344f30b39497c8a1a
thisLight <l1589002388@gmail.com>
2020-10-28 21:35:05 +0800

    hussar: only close 'close' connection if thread is dead

--- src/hussar.lua
+++ src/hussar.lua
@@ -38,7 +38,7 @@ local function hussar_managing_thread(hussar)
                 conn:close('timeout')
                 table.insert(remove_later_index, i)
             elseif coroutine.status(binded_thread) == 'dead' then
-                conn:close("thread is dead")
+                if not conn:is_keep_alive() then conn:close("thread is dead") end
                 table.insert(remove_later_index, i)
             elseif not conn:is_alive() then
                 table.insert(remove_later_index, i)
