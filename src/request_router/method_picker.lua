510f640710e3a0064648f8e9546682075de10586
thisLight <l1589002388@gmail.com>
2020-11-08 23:23:47 +0800

    request_router.method_picker: can automatically respond options

--- src/request_router/method_picker.lua
+++ src/request_router/method_picker.lua
@@ -1,10 +1,24 @@
-local respond = require('httputil').respond
+local respond = require('hussar.httputil').respond
+local tools = require 'hussar.request_router.tools'
 local lower = string.lower
+local build_options_response = tools.build_options_response
 
 return function(options)
+    local supported_methods = {}
+    for k, _ in pairs(options) do
+        if k ~= 'default' then
+            supported_methods[#supported_methods+1] = k
+        end
+    end
     return function(request, frame, ...)
         local method = lower(request.method)
-        if options[method] then
+        if method == 'options' then
+            if options.options then
+                return options.options(request, ...)
+            else
+                respond(frame.connection, build_options_response(supported_methods))
+            end
+        elseif options[method] then
             return options[method](request, ...)
         elseif options.default then
             return options.default(request, ...)
